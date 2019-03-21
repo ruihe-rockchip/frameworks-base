7a31d2b01c6a7541674181ab4d995aeb2eeeb201
Xiao Ya peng <yp.xiao@rock-chips.com>
2019-03-21 08:42:39 +0800

    Revert "Exit touch mode on mouse/stylus click"
    This reverts commit 7614cdf9c39039e6aaf5a896006445046003215a.

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -4536,11 +4536,11 @@ public final class ViewRootImpl implements ViewParent,
                 mTranslator.translateEventInScreenToAppWindow(event);
             }
 
-            // Enter touch mode on down or scroll, if it is coming from a touch screen device,
-            // exit otherwise.
+            // Enter touch mode on down or scroll.
             final int action = event.getAction();
-            if (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_SCROLL) {
-                ensureTouchMode(event.isFromSource(InputDevice.SOURCE_TOUCHSCREEN));
+            if (event.isFromSource(InputDevice.SOURCE_TOUCHSCREEN)
+                    && (action == MotionEvent.ACTION_DOWN || action == MotionEvent.ACTION_SCROLL)) {
+                ensureTouchMode(true);
             }
 
             if (action == MotionEvent.ACTION_DOWN && mAttachInfo.mTooltipHost != null) {
